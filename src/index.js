e7cb4827b4a3fc8aab42707f7be3962662f9b30c
RobocrafterLP <60442646+RobocrafterLP@users.noreply.github.com>
2022-03-23 05:28:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ const app = express()
 
 app.use(express.json())
 
-const sub = require('./routes/test')
-app.use('/', sub)
-
-app.listen(3000, () => console.log("Online"))
+//const sub = require('./routes/test')
+//app.use('/', sub)
 
 app.get('/', (req, res) => {
   req.send("Please Add a user id after /")
 })
+app.listen(3000, () => console.log("Online"))
